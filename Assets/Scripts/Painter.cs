d363abc852bfa12f96d5861ae810d2f0915bfeba
Junghoon Choi <be9904@gmail.com>
2023-04-16 00:22:25 +0900

    implement painter move with lever

--- Assets/Scripts/Painter.cs
+++ Assets/Scripts/Painter.cs
@@ -6,23 +6,54 @@ namespace AttnKare
 {
     public class Painter : MonoBehaviour
     {
-        private static readonly int triggerID = Animator.StringToHash("Trigger_Painter");
+        [SerializeField] private BNG.Lever painterControl;
+        [SerializeField] private Transform upperLimit;
+        [SerializeField] private Transform lowerLimit;
+        [SerializeField] private float painterActivationThreshold;
+        [SerializeField] private float painterMoveSpeed;
 
-        private Animator painterAnimator;
-        private AnimatorClipInfo[] animationClips;
-
-        // Start is called before the first frame update
-        void Start()
-        {
-            painterAnimator = GetComponent<Animator>();
-            animationClips = painterAnimator.GetCurrentAnimatorClipInfo(0);
-        }
+        private float timer;
+        private bool leverUp;
+        private bool leverDown;
 
         // Update is called once per frame
-        void Update()
+        void Update() => MovePainter();
+
+        void MovePainter()
         {
-            if(Input.GetKeyDown(KeyCode.LeftControl))
-                painterAnimator.SetTrigger(triggerID);
+            if (leverDown && painterControl.LeverPercentage < painterActivationThreshold)
+                timer -= Time.deltaTime * painterMoveSpeed;
+
+            if (leverUp && 100f - painterControl.LeverPercentage < painterActivationThreshold)
+                timer += Time.deltaTime * painterMoveSpeed;
+
+            if (timer < 0)
+            {
+                timer = 0;
+            }
+
+            if (timer > 1)
+            {
+                timer = 1;
+            }
+            
+            if (timer >= 0 && timer <= 1)
+            {
+                transform.localPosition = Vector3.Lerp(lowerLimit.localPosition , upperLimit.localPosition,
+                    timer);
+            }
+        }
+
+        public void OnLeverUp()
+        {
+            Debug.Log("Lever Up");
+            leverUp = true;
+        }
+
+        public void OnLeverDown()
+        {
+            Debug.Log("Lever Down");
+            leverDown = true;
         }
     }
 }
